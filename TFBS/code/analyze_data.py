6ae0e8a96d11db631fcc1c0935d6e908e164129a
Maryam-Haghani <haghani@vt.edu>
2025-04-09 14:19:13 -0400

    Use custom logger

--- TFBS/code/analyze_data.py
+++ TFBS/code/analyze_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
-import logging
 from collections import Counter
 import argparse
+from logger import CustomLogger
 
 # python analyze_data.py --generated_data_file '../inputs/AtABFs_training_shuffle_neg_stride_200.csv' --species "At"
 # python analyze_data.py --generated_data_file '../inputs/SiABFs_training_shuffle_neg_stride_200.csv' --species "Si"
-# python analyze_data.py --generated_data_file '../inputs/Josey-AtABF2_training_shuffle_neg_stride_200.csv' --dataset "ABF2-Josey"
-# python analyze_data.py --generated_data_file '../inputs/Ronan-AtABF2_training_shuffle_neg_stride_200.csv' --dataset "ABF2-Ronan"
+# python analyze_data.py --generated_data_file '../inputs/Josey-AtABF2_training_shuffle_neg_stride_201.csv' --dataset "ABF2-Josey-201"
+# python analyze_data.py --generated_data_file '../inputs/Ronan-AtABF2_training_shuffle_neg_stride_201.csv' --dataset "ABF2-Ronan-201"
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Analyze generated dataset")
@@ -15,62 +15,54 @@ def parse_arguments():
     parser.add_argument("--dataset", type=str, required=True)
     return parser.parse_args()
 
-def create_logger(log_file):
-  logger = logging.getLogger(__name__)
-  logger.setLevel(logging.DEBUG)
-
-  file_handler = logging.FileHandler(log_file)
-  file_handler.setLevel(logging.DEBUG)
-
-  console_handler = logging.StreamHandler()
-  console_handler.setLevel(logging.DEBUG)
-
-  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-  file_handler.setFormatter(formatter)
-  console_handler.setFormatter(formatter)
-
-  logger.addHandler(file_handler)
-  logger.addHandler(console_handler)
-  
-  return logger
-
-  logger.addHandler(file_handler)
-
 def count_nucleotide(df):
     # Concatenate all sequences into one string
     all_sequences = df['sequence'].str.cat(sep='')
 
     # Count the frequency of each nucleotide
     frequency = Counter(all_sequences)
-    return dict(frequency)
+
+    # Calculate total number of nucleotides
+    total = sum(frequency.values())
+
+    # Convert counts to proportions (percentages) and round to 2 decimal places
+    proportions = {base: round((count / total) * 100, 2) for base, count in frequency.items()}
+
+    return dict(frequency), proportions
 
 
 args = parse_arguments()
 
 df = pd.read_csv(args.generated_data_file)
 
-logger = create_logger(f'../outputs/logs/data_{args.species}-{args.dataset}.log')
+logger = CustomLogger(__name__, log_directory=f'../outputs/logs', log_file = f'log_data_{args.species}-{args.dataset}.log')
 
 # ALL DATA
-logger.info("************** ALL **************")
-logger.info(f"shape: {df.shape}")
+logger.log_message("************** ALL **************")
+logger.log_message(f"shape: {df.shape}")
 df_len = df['sequence'].str.len()
-logger.info(f"statistics:{df_len.describe().round(2)}")
-logger.info(f"nucleotide count:{count_nucleotide(df)}")
+logger.log_message(f"statistics:{df_len.describe().round(2)}")
+count, prop = count_nucleotide(df)
+logger.log_message(f"nucleotide count:{count}")
+logger.log_message(f"nucleotide proportions:{prop}")
 
 
 # POSITIVE DATA
-logger.info("************** POSITIVE **************")
+logger.log_message("************** POSITIVE **************")
 pos_df = df[df['label'] == 1]
-logger.info(f"shape: {pos_df.shape}")
+logger.log_message(f"shape: {pos_df.shape}")
 pos_len = pos_df['sequence'].str.len()
-logger.info(f"statistics:{pos_len.describe().round(2)}")
-logger.info(f"nucleotide count:{count_nucleotide(pos_df)}")
+logger.log_message(f"statistics:{pos_len.describe().round(2)}")
+count, prop = count_nucleotide(pos_df)
+logger.log_message(f"nucleotide count:{count}")
+logger.log_message(f"nucleotide proportions:{prop}")
 
 # NEGATIVE DATA
-logger.info("************** NEGATIVE **************")
+logger.log_message("************** NEGATIVE **************")
 neg_df = df[df['label'] == 0]
-logger.info(f"shape: {neg_df.shape}")
+logger.log_message(f"shape: {neg_df.shape}")
 neg_len = neg_df['sequence'].str.len()
-logger.info(f"statistics:{neg_len.describe().round(2)}")
-logger.info(f"nucleotide count:{count_nucleotide(neg_df)}")
+logger.log_message(f"statistics:{neg_len.describe().round(2)}")
+count, prop = count_nucleotide(neg_df)
+logger.log_message(f"nucleotide count:{count}")
+logger.log_message(f"nucleotide proportions:{prop}")
